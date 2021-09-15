0bf072eeaeec389d8407e76238ebd8716566b5b5
KatlegoMashiane <Katlego.timothy.9@gmail.com>
2021-09-15 19:21:44 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BizwheelAppSummative.Models;
+using System.Data.SqlClient;
+using Microsoft.VisualStudio.Services.Account;
+
+namespace BizwheelAppSummative.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Service()
+        {
+            return View();
+        }
+
+        public ActionResult Portfolio()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        
+        public ActionResult Login()
+
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BizwheelAppSummative.MvcApplication" Language="C#" %>
